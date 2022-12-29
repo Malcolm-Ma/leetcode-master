4134e92af52b5db0368227cfabe48c5fb89a3cc1
Mingze Ma <mingzema.1998@foxmail.com>
2022-12-29 17:22:24 +0800

    Definition of linked list

--- linkedList/ListNode.java
+++ linkedList/ListNode.java
@@ -4,8 +4,8 @@ package linkedList;
  * Definition for singly-linked list.
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     ListNode() {
     }
